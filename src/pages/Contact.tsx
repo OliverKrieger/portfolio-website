496e0418fff5582b4f0cd8c0275a8c482f6d2d9a
Oliver Krieger <oliver.krieger76@gmail.com>
2024-09-14 10:01:37 +0100

    add icons

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,27 @@
+import LinkedInIcon from '../assets/icons/i_linkin.svg?react';
+import EmailIcon from '../assets/icons/i_email.svg?react';
+import GithubIcon from '../assets/icons/i_github.svg?react';
+import LocationIcon from '../assets/icons/i_location.svg?react';
+import PhoneIcon from '../assets/icons/i_phone.svg?react';
+
 const Contact = () => {
     return (
         <div className="flex justify-center animate-slideInLeft">
-            <h1>Contact Page</h1>
-            <p>Get in touch with us!</p>
+            <div className="flex flex-col text-center ...">
+                <h2 className="text-lg font-bold">Oliver Krieger</h2>
+                <h3>Software Developer</h3>
+                <div className="links-container">
+                    <LinkedInIcon className="w-6 h-6 text-blue-500" />
+                    <EmailIcon className="w-6 h-6 text-blue-500" />
+                    <GithubIcon className="w-6 h-6 text-blue-500" />
+                    <LocationIcon className="w-6 h-6 text-blue-500" />
+                    <PhoneIcon className="w-6 h-6 text-blue-500" />
+                </div>
+                <div className="info-container">
+
+                </div>
+                <button>Download CV</button>
+            </div>
         </div>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
         <div className="animate-slideInLeft h-full ...">
             <div className='home-container flex flex-col justify-center text-center ... h-full ... -mt-8'>
                 <div className='logo-container flex justify-center'>
-                    <Logo width_val={312} height_val={312}/>
+                    <Logo width_val={312} height_val={312} />
                 </div>
                 <div className='content-container'>
                     <h1 className='text-4xl text-bisque-500 tracking-[.25em] uppercase ... font-verdana custom-shadow-white text-stroke'>Oliver Krieger</h1>
